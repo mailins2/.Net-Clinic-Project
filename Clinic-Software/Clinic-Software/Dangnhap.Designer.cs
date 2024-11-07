b920ab336e50d94555386e5b4c5515a517777cfb
Nguyen Mai Linh <nguyenmailinhnana@gmail.com>
2024-11-07 15:31:46 +0700

    recreate project

---
+++ Clinic-Software/Clinic-Software/Dangnhap.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Clinic_Software
+{
+    partial class Dangnhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblchao = new System.Windows.Forms.Label();
+            this.chkbMatKhau = new System.Windows.Forms.CheckBox();
+            this.txtmatkhau = new System.Windows.Forms.TextBox();
+            this.lblMatKhau = new System.Windows.Forms.Label();
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblchao
+            // 
+            this.lblchao.AutoSize = true;
+            this.lblchao.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblchao.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.lblchao.Location = new System.Drawing.Point(115, 47);
+            this.lblchao.Name = "lblchao";
+            this.lblchao.Size = new System.Drawing.Size(267, 27);
+            this.lblchao.TabIndex = 9;
+            this.lblchao.Text = "Chào mừng đăng nhập";
+            // 
+            // chkbMatKhau
+            // 
+            this.chkbMatKhau.AutoSize = true;
+            this.chkbMatKhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.chkbMatKhau.Location = new System.Drawing.Point(168, 152);
+            this.chkbMatKhau.Name = "chkbMatKhau";
+            this.chkbMatKhau.Size = new System.Drawing.Size(130, 20);
+            this.chkbMatKhau.TabIndex = 8;
+            this.chkbMatKhau.Text = "Hiển thị mật khẩu";
+            this.chkbMatKhau.UseVisualStyleBackColor = true;
+            // 
+            // txtmatkhau
+            // 
+            this.txtmatkhau.Location = new System.Drawing.Point(168, 105);
+            this.txtmatkhau.Multiline = true;
+            this.txtmatkhau.Name = "txtmatkhau";
+            this.txtmatkhau.PasswordChar = '*';
+            this.txtmatkhau.Size = new System.Drawing.Size(229, 32);
+            this.txtmatkhau.TabIndex = 7;
+            // 
+            // lblMatKhau
+            // 
+            this.lblMatKhau.AutoSize = true;
+            this.lblMatKhau.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblMatKhau.Location = new System.Drawing.Point(63, 108);
+            this.lblMatKhau.Name = "lblMatKhau";
+            this.lblMatKhau.Size = new System.Drawing.Size(80, 19);
+            this.lblMatKhau.TabIndex = 6;
+            this.lblMatKhau.Text = "Mật khẩu";
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.AutoSize = true;
+            this.btnDangNhap.BackColor = System.Drawing.Color.SkyBlue;
+            this.btnDangNhap.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnDangNhap.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnDangNhap.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnDangNhap.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnDangNhap.Location = new System.Drawing.Point(160, 202);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(138, 40);
+            this.btnDangNhap.TabIndex = 5;
+            this.btnDangNhap.Text = "Đăng nhập";
+            this.btnDangNhap.UseVisualStyleBackColor = false;
+            // 
+            // Dangnhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(460, 274);
+            this.Controls.Add(this.lblchao);
+            this.Controls.Add(this.chkbMatKhau);
+            this.Controls.Add(this.txtmatkhau);
+            this.Controls.Add(this.lblMatKhau);
+            this.Controls.Add(this.btnDangNhap);
+            this.Name = "Dangnhap";
+            this.Text = "Dangnhap";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblchao;
+        private System.Windows.Forms.CheckBox chkbMatKhau;
+        private System.Windows.Forms.TextBox txtmatkhau;
+        private System.Windows.Forms.Label lblMatKhau;
+        private System.Windows.Forms.Button btnDangNhap;
+    }
+}
---
+++ Clinic-Software/Clinic-Software/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clinic_Software
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Homepage());
+            //Application.Run(new DienThongTinBenhNhan());
+            //Application.Run(new Dangnhap());
+            //Application.Run(new Doimatkhau());
+        }
+    }
+}
